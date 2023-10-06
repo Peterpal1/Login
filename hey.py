e31509e4cfab840b94d39cec5e68b969134dbc86
Jwanrogers1 <nicahrogers20@gmail.com~>
2023-10-06 20:58:57 +0300

    message

---
+++ hey.py
@@ -0,0 +1,34 @@
+print ("  _________")
+print (" /         \\")
+print ("/           \\")
+print ("\           /")
+print (" \         /")
+print ("  ---------")
+
+
+
+
+
+#next task
+print ("\           /")
+print (" \         /")
+print ("  ---------")
+print ("  +--------+")
+
+
+
+#next task
+
+print("  _________")
+print(" /         \\")
+print("/           \\")
+print("|    STOP    |")
+print("\\           /")
+print(" \\         /")
+print("  ---------")
+
+
+#next task
+print("  _________")
+print(" /         \\")
+print("/           \\")
